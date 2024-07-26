52a3eaba186236c223bfdbb2fb7872df824cd6d8
Priyanshi Nama <priyanshinama@fico.com>
2024-07-26 16:35:55 +0530

    Working

--- src/main/java/com/example/spring_boot/controller/LoanController.java
+++ src/main/java/com/example/spring_boot/controller/LoanController.java
@@ -2,12 +2,14 @@ package com.example.spring_boot.controller;
 
 import com.example.spring_boot.model.LoanApplication;
 import com.example.spring_boot.repository.LoanApplicationRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.AotInitializerNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/phansbank/v1/")
@@ -18,7 +20,7 @@ public class LoanController {
 
     //	get all applcn
     @GetMapping("/view")
-    public List<LoanApplication> getAllIDs(){
+    public List<LoanApplication> getAllIDs() {
         return loanRepo.findAll();
     }
 
@@ -31,13 +33,10 @@ public class LoanController {
     //GET APPCN BY ID-path variable : to view applcn by id
     @GetMapping("/view/{id}")
     public ResponseEntity<LoanApplication> getApplicationById(@PathVariable Long id) {
-        LoanApplication data= loanRepo.findById(id)
+        LoanApplication data = loanRepo.findById(id)
                 .orElseThrow(() -> new AotInitializerNotFoundException(null,
                         "Application doesn't exist for id"));
         return ResponseEntity.ok(data);
     }
-
-    //update
-    //delete
 }
 
