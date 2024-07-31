7baf4c5f18265e211c58889c26f1bfc82fab6d18
Priyanshi Nama <priyanshinama@fico.com>
2024-07-31 15:39:37 +0530

    merging

--- src/test/java/TestFiles/LoanControllerTest.java
+++ src/test/java/TestFiles/LoanControllerTest.java
@@ -5,12 +5,14 @@
 //import com.example.spring_boot.repository.LoanApplicationRepository;
 //import org.junit.jupiter.api.BeforeEach;
 //import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 ////import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.http.MediaType;
 //import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
 //import org.springframework.test.web.servlet.MockMvc;
 //
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,6 +21,7 @@
 //import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 //
 //@SpringBootTest(classes = Application.class)
+//@ExtendWith(SpringExtension.class)
 //@AutoConfigureMockMvc
 //@ActiveProfiles("test")
 //public class LoanControllerTest {
